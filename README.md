eb88aa8c6946827498bd3554bf071a49198b710a
aestheticJL <m258789@126.com>
2020-04-08 00:27:21 +0800

    :sparkles:现在可以动态查询豆瓣api了

---
+++ README.md
@@ -0,0 +1,4 @@
+# Getting Started
+[豆瓣api文档](https://douban-api-docs.zce.me/movie.html#search)
+根据id搜索电影api:
+http://api.douban.com/v2/movie/subject/{id}?apikey=0df993c66c0c636e29ecbb5344252a4a
--- src/main/java/com/mmt/myowndoubanspringboot/demo/controller/SpringRestTemplateController.java
+++ src/main/java/com/mmt/myowndoubanspringboot/demo/controller/SpringRestTemplateController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,9 +14,9 @@ public class SpringRestTemplateController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/testGetApi")
-    public String getJson() {
-        String url = "http://api.douban.com/v2/movie/new_movies?apikey=0df993c66c0c636e29ecbb5344252a4a";
+    @GetMapping("/testGetApi/{id}")
+    public String getJson(@PathVariable String id) {
+        String url = "http://api.douban.com/v2/movie/subject/"+id+"?apikey=0df993c66c0c636e29ecbb5344252a4a";
         //String json =restTemplate.getForObject(url,Object.class);
         ResponseEntity<String> results = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
         String json = results.getBody();
